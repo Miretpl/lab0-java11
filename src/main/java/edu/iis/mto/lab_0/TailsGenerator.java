121c12c4c8c297fdab93c3c3d6e7b70f5a65eed0
Przemysław Mirowski <215825@edu.p.lodz.pl>
2020-03-02 18:49:46 +0100

    lab0 - poprawa formatowania

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,14 +6,10 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-
         ArrayList<String> tailList = new ArrayList<>(value.length());
-
         for (int i = 0; i < value.length(); i++)
             tailList.add(value.substring(i, value.length()));
-
         tailList.add("");
-
         return tailList;
     }
 }
