711c25e07755d62990181617a939b88bf11dc1af
Przemysław Mirowski <215825@edu.p.lodz.pl>
2020-03-02 18:57:58 +0100

    lab0 - dodanie nawiasow klamrowych

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -7,8 +7,9 @@ public class TailsGenerator {
 
     public List<String> tails(String value) {
         ArrayList<String> tailList = new ArrayList<>(value.length());
-        for (int i = 0; i < value.length(); i++)
+        for (int i = 0; i < value.length(); i++) {
             tailList.add(value.substring(i, value.length()));
+        }
         tailList.add("");
         return tailList;
     }
