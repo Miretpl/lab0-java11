01abfbcd35e2509baf0e0f6b9e4ae1674d2f66c3
Przemysław Mirowski <215825@edu.p.lodz.pl>
2020-03-02 18:22:45 +0100

    lab0

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
-    }
 
+        ArrayList<String> tailList = new ArrayList<>(value.length());
+
+        for (int i = 0; i < value.length(); i++)
+            tailList.add(value.substring(i, value.length()));
+
+        tailList.add("");
+
+        return tailList;
+    }
 }
